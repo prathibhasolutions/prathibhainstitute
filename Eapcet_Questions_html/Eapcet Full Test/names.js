11599e9baabbec9d3508b87db5042f12ede7a32c
Shashank3004 <167780160+Shashank3004@users.noreply.github.com>
2025-03-24 15:17:05 +0530

    new1

--- Eapcet_Questions_html/Eapcet Full Test/names.js
+++ Eapcet_Questions_html/Eapcet Full Test/names.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-let students = ["sravan", "shashank", "akhilesh","rishika"];
+let students = ["sravan", "shashank", "akhilesh","rishika","Hasini","Spoorthi","Sharanya","Spandhana"];
 localStorage.setItem("students", JSON.stringify(students));
 
 
