5ad1b393f547c4c440990222850e25f70d60da3a
Shashank3004 <167780160+Shashank3004@users.noreply.github.com>
2025-03-25 19:25:22 +0530

    fulll test 2 script change

--- Eapcet_Questions_html/Eapcet Full Test/names.js
+++ Eapcet_Questions_html/Eapcet Full Test/names.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-let students = ["teja sri", "hasini"];
+let students = ["sravan", "rishika", "hasini", "spoorthi", "sharanya", "spandhana",
+     "srinidhi", "anugna", "teja sri", "sai hansini", "shashank", "akhilesh"];
 
 localStorage.setItem("students", JSON.stringify(students));
 
-// "sravan", "rishika", "hasini", "spoorthi", "sharanya", "spandhana",
-//      "srinidhi", "anugna", "teja sri", "sai hansini", "shashank", "akhilesh"
+
 
 
 }
