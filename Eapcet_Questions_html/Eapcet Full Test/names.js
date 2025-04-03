4d65db3253f4c92801a7f7cdc97590a83fbabdb7
Shashank3004 <167780160+Shashank3004@users.noreply.github.com>
2025-04-03 15:05:04 +0530

    new1

--- Eapcet_Questions_html/Eapcet Full Test/names.js
+++ Eapcet_Questions_html/Eapcet Full Test/names.js
@@ -1,5 +1,5 @@
 function isStudentAllowed(studentName) {
-    const allowedStudents = ["sravan", "shashank", "Akhilesh","spandhana","p.vaishnavi","sharanya","Hasini","akshaya","anugna","N.Vaishnavi","spoorthi","hansini","rishika","srinidhi","Teja sri","jashwanth","Harshith","Vishnu","manish","srinith"]; // Add more names
+    const allowedStudents = ["sravan", "shashank", "Akhilesh","spandhana","p.vaishnavi","sharanya","Hasini","akshaya","anugna","N.Vaishnavi","spoorthi","hansini","rishika","srinidhi","Teja sri","jashwanth","Harshith","Vishnu","manish","srinith","Karunya","Anshul Reddy","Nihith Reddy"]; // Add more names
 
     return allowedStudents.some(name => name.toLowerCase() === studentName.toLowerCase());
 }
